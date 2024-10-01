8438e36be7c8a0b559557ec41f397ad60d356cde
Namira Nurfaliani <129768774+iranamira@users.noreply.github.com>
2024-10-01 11:35:37 +0700

    Update NonMemberDiscountStrategy.java

--- NonMemberDiscountStrategy.java
+++ NonMemberDiscountStrategy.java
@@ -1,7 +1,7 @@
 public class NonMemberDiscountStrategy implements DiscountStrategy {
     @Override
     public double getDiscountedPrice(double price) {
-        // Diskon 5% untuk non-member
+    
         return price * 0.95;
     }
 }
