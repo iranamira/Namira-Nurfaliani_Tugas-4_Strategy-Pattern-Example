4d0f218e9070f138157b17b993fa8463a56c4200
Namira Nurfaliani <129768774+iranamira@users.noreply.github.com>
2024-10-01 11:35:19 +0700

    Update MemberDiscountStrategy.java

--- MemberDiscountStrategy.java
+++ MemberDiscountStrategy.java
@@ -1,8 +1,7 @@
-// MemberDiscountStrategy.java
 public class MemberDiscountStrategy implements DiscountStrategy {
     @Override
     public double getDiscountedPrice(double price) {
-        // Diskon 10% untuk member
+  
         return price * 0.9;
     }
 }
