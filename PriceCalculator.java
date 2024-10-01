f00c0f34866a47fcfa4601f186ff6a5ab69f517e
Namira Nurfaliani <129768774+iranamira@users.noreply.github.com>
2024-10-01 11:35:54 +0700

    Update PriceCalculator.java

--- PriceCalculator.java
+++ PriceCalculator.java
@@ -1,13 +1,12 @@
-// PriceCalculator.java
 public class PriceCalculator {
     private DiscountStrategy discountStrategy;
 
-    // Setter untuk mengganti strategi diskon
+ 
     public void setDiscountStrategy(DiscountStrategy discountStrategy) {
         this.discountStrategy = discountStrategy;
     }
 
-    // Metode untuk menghitung harga diskon
+   
     public double calculatePrice(double price) {
         if (discountStrategy == null) {
             throw new IllegalStateException("DiscountStrategy belum diset.");
